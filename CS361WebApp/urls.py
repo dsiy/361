398edead616cb7d2aa7d7e709c2fb7eeb0491727
VJTotka <vjtotka@uwm.edu>
2019-11-03 11:00:13 -0600

    updated command for info flow
    prints out input from command line

--- CS361WebApp/urls.py
+++ CS361WebApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='CS361WebApp-home'),
+  path('', views.TAManager.as_view(), name='CS361WebApp-home'),
   path('about/', views.about, name='CS361WebApp-about'),
 ]
--- CS361WebApp/views.py
+++ CS361WebApp/views.py
@@ -3,12 +3,15 @@ from django.views import View
 from django.http import HttpResponse
 
 #this still needs to be integrated once we make a class to take input
+from CS361WebApp.models import InputManager
+
+
 class TAManager(View):
     def get(self, request):
         return render(request, 'main/index.html')
 
     def post(self, request):
-        yourInstance = YourClass()
+        yourInstance = InputManager()
         commandInput = request.POST["command"]
         if commandInput:
             response = yourInstance.command(commandInput)
@@ -17,6 +20,7 @@ class TAManager(View):
         return render(request, 'main/index.html', {"message": response})
 
 #this works but is just test data, still needs call and response
+
 def home(request):
     return render(request, "main/index.html")
 
