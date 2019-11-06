1f460e11eff8be87641b42b4b4bc7aa604014ddd
David Krampert <davidakrampert@gmail.com>
2019-11-06 17:08:24 -0600

    Merge branch 'master' of C:\Users\super\Desktop\we-dont-like-krique-real with conflicts.

--- CS361WebApp/models.py
+++ CS361WebApp/models.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 
@@ -9,21 +8,11 @@ class Account(models.Model):
 
     # Returns True if account was succesfully logged in
     # Throws an error is the account is not valid
-    def login(self, password):
+    def Login(self, password):
         pass
 
-    def logout(self):
+    def Logout(self):
         pass
-
-    def __str__(self):
-        return self.email
-
-
-class loggedIn(models.Model):
-    email = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.email
 
 
 class Administrator(Account):
@@ -65,29 +54,66 @@ class InputManager:
         first = commands[0].lower()
         # login
         if first == "login":
-            # check size of inputs v
-            # check email and password v
-            # check if there is duplicate
-            if (len(commands) < 3) | (not Account.objects.filter(email=commands[1]).exists()):
-                output = "No Such User Exists!"
-            else:
-                # add to loggedIn table v
-                comein = loggedIn(email=commands[1])
-                comein.save()
-                output = "Log-in Success!"
-
+            # check size of inputs
+            #a = CourseTimeValidator()
+            # if a.validate(command):
+            output = "login successful"
+            # check email and password
+            output = "login failed"
 
         # logout
         if first == "logout":
             # do thing
-            if ((not Account.objects.filter(email=commands[1]).exists())
-                    | (not loggedIn.objects.filter(email=commands[1]).exists())):
-                output = "You are not logged in!"
-            else:
-                getout = loggedIn.objects.get(email=commands[1])
-                getout.delete()
-                output = "Log-out Success!"
-        # just for checking
-        output = loggedIn.objects.all()
+            if Account.Logout():
+                output = "logout successful"
+
+        if first == "AddClass":
+          #  a = CourseTimeValidator()
+           # if a.validate(command):
+                output = "Class Successfully Added!"
         # create class command
+        return output
+
+class CourseTimeValidator:  # takes in string. addClass <1> <2> <3>...<n>
+    def validator(self, input):
+        output = True
+        list = input.split()
+        if not len(list) == 7 | list.len() == 8:
+            output = False
+
+        count = 0
+        for a in list:
+            if count == 0:
+                count += 1
+
+            elif count == 1:
+                if (a.isnumeric()) == True:
+                    output = False
+                count += 1
+
+            elif count == 2:
+                if a.isalpha() == True:
+                    output = False
+                count += 1
+
+            elif count == 3:
+                if a.isalpha() == True:
+                    output = False
+                count += 1
+
+            elif count == 4:
+                if a.isalpha() == True:
+                    output = False
+                count += 1
+
+            elif count == 5:
+                if (a.isnumeric()) == True:
+                    output = False
+                count += 1
+
+            elif count == 6:
+                if a.isalpha() == True:
+                    output = False
+                count += 1
+
         return output
