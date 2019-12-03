f6d014a84d8e5a85d57db2a77c080ed357b698b2
Hojin Park <hojin@uwm.edu>
2019-12-03 14:53:25 -0600

    class view + addclass version 1.0

--- CS361WebApp/views.py
+++ CS361WebApp/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import auth
-from CS361WebApp.models import InputManager
+from CS361WebApp.models import CourseTime
 from django.contrib.auth.decorators import login_required, user_passes_test
 from CS361WebApp.forms import CourseTimeForm
 
 def admin_check(user):
     return user.is_staff
 
+lists = {
+        'CS361',
+        'CS351',
+        'CS458',
+        }
 @login_required
 def home(request):
-    manager = InputManager()
-    commandInput = request.POST
-    if commandInput:
-        response = manager.command(commandInput)
-    else:
-        response = ""
-    return render(request, 'CS361WebApp/home.html', {"message": response})
+    # course_result = CourseTime.objects.all()
+    course_result = lists
+    return render(request, 'CS361WebApp/home.html', {"courses": course_result})
 
 @login_required
 @user_passes_test(admin_check)
