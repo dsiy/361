141edd0a2229ccdbedeb6a534e728b9241a5f9a9
Hojin Park <hojin@uwm.edu>
2019-12-04 15:48:25 -0600

    small changes in models.py

--- CS361WebApp/models.py
+++ CS361WebApp/models.py
@@ -3,80 +3,6 @@ from django.db import models
 
 
 # Create your models here.
-class Account(models.Model):
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    loggedIn = models.BooleanField(default=False)
-
-    # Returns True if account was succesfully logged in
-    # Throws an error is the account is not valid
-    def login(self, password):
-
-        if self.password != password:
-            output = "Your password is incorrect!"
-
-        elif self.loggedIn:
-            output = "You are already logged in!"
-
-        else:
-            # add to loggedIn table v
-            self.loggedIn = True
-            self.save()
-            output = "Log-in Success!"
-
-        return output
-
-    def logout(self):
-        if not self.loggedIn:
-            output = "You are not logged in!"
-        else:
-            self.loggedIn = False
-            self.save()
-            output = "Log-out Success!"
-
-        return output
-
-    def __str__(self):
-        return self.email
-
-    def get_password(self):
-        return self.password
-
-
-class Administrator(Account):
-    def resetPassword(self):
-        pass
-
-    # Returns True if class was successfully added to the classList and False if there is an error adding the class
-    def addClass(self, className):
-        pass
-
-    # Returns True if class was successfully removed to the classList and False if there is an error removing the class
-    def removeClass(self, className):
-        pass
-
-
-class TA(Account):
-    def fill_Info(self):
-        pass
-
-    def addClass(self, className, priority):
-        pass
-
-    def viewPriorityList(self):
-        pass
-
-    def changePriority(self, className, priority):
-        pass
-
-    def removeClass(self, className):
-        pass
-
-
-class Instructor(Account):
-    pass
-
-
 class CourseTime(models.Model):
     department = models.CharField(max_length=50)
     number = models.CharField(max_length=50)
@@ -86,46 +12,8 @@ class CourseTime(models.Model):
     section = models.CharField(max_length=50)
     instructor = models.CharField(max_length=50, blank = True)
 
-
-class InputManager:
-    def command(self, command):
-        output = command
-        commands = command.split()
-        first = commands[0].lower()
-
-        # login
-        if first == "login":
-            # check size of inputs v
-            # check email and password v
-            # check if there is duplicate v
-
-            if (len(commands) < 3) | (not Account.objects.filter(email=commands[1]).exists()):
-                output = "No Such User Exists!"
-            elif Account.objects.filter(email=commands[1]).exists():
-                try:
-                    output = Account.objects.filter(email=commands[1])[0].login(commands[2])
-                except Account.DoesNotExist:
-                    output = "No Such User Exists!"
-        # logout
-        if first == "logout":
-            # if logged in
-            if len(commands) < 2 | (not Account.objects.filter(email=commands[1]).exists()):
-                output = "No Such User Exists!"
-            else:
-                output = Account.objects.filter(email=commands[1])[0].logout()
-
-        if first == "addClass":
-            cv = CourseTimeValidator()
-            if cv.validator(command):
-                output = "Class successfully created!"
-            else:
-                output = "Invalid Class!"
-        # just for checking
-        # output =  str(loggedIn.objects.all()) + "   " + output
-        # if loggedIn.objects.all(): output = 'wahhh!!'
-
-        # create class command
-        return output
+    def __str__(self):
+        return self.department + " " + self.number
 
 
 class CourseTimeValidator:  # takes in string. addClass <1> <2> <3>...<n>
