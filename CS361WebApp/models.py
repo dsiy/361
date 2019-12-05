ff0e9c74bbd9258f5fe822a782434939beca18de
Danielsiy <dsiy@wisc.edu>
2019-12-04 22:20:01 -0600

    Validation done

--- CS361WebApp/models.py
+++ CS361WebApp/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+from django.template.defaultfilters import slugify
+
+
 class CourseTime(models.Model):
     department = models.CharField(max_length=50)
     number = models.CharField(max_length=50)
