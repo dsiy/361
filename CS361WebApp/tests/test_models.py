24c8d64973bbdc8d97ef7d19f49774b452a985de
VJTotka <vjtotka@uwm.edu>
2019-12-14 15:54:41 -0600

    Revert "Merge branch 'master' of https://bitbucket.org/radojev3/we-dont-like-krique-real"
    This reverts commit 1bf9719a81a6d1ff5693014fb24cd2ed77b90763, reversing
    changes made to 53244f83b5b997ffcf1832059d6ac15dbc546bd8.

--- CS361WebApp/tests/test_models.py
+++ CS361WebApp/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from CS361WebApp.models import User, CourseTime, CreatePriority, SavePriority
+from CS361WebApp.models import User
 
 
 class UserTestCase(TestCase):
@@ -245,7 +245,7 @@ class TATestCase(TestCase):
         self.assertEqual(santha.addClass("CS361", 1), True)
         self.assertEqual(santha.addClass("CS337", 2), True)
         self.assertEqual(santha.addClass("CS395", 3), True)
-        self.assertEqual(santha.viewPriority(), list)
+        self.assertEqual(santa.viewPriority(), list)
 
     def test_TA_priority_create2(self):
         User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
@@ -254,7 +254,7 @@ class TATestCase(TestCase):
         self.assertEqual(santha.addClass("cs361", 1), True)
         self.assertEqual(santha.addClass("cS337", 2), True)
         self.assertEqual(santha.addClass("CS395", 3), True)
-        self.assertEqual(santha.viewPriority(), list)
+        self.assertEqual(santa.viewPriority(), list)
 
     def test_TA_priority_create3(self):
         User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
@@ -267,7 +267,7 @@ class TATestCase(TestCase):
         self.assertEqual(santha.addClass("CS420", 2), False)
         self.assertEqual(santha.addClass("CS337", 2), True)
         self.assertEqual(santha.addClass("CS395", 3), True)
-        self.assertEqual(santha.viewPriority(), list)
+        self.assertEqual(santa.viewPriority(), list)
 
     def test_TA_priority_view_empty(self):
         User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
@@ -362,90 +362,6 @@ class TATestCase(TestCase):
         self.assertEqual(santha.changePriority("CS 337", 1), True)
         self.assertEqual(santha.changePriority("CS 395", 1), True)
         self.assertEqual(santha.viewPriority(), list)
-
-    def test_create_priority1(self):
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        self.assertEquals(CreatePriority.objects.get(classes=c361), c1)
-
-    def test_create_priority2(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        self.assertEquals(CreatePriority.objects.get(classes=c395), None)
-
-    def test_create_priority3(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        c2 = CreatePriority.objects.create(classes=c458, priority="3")
-        self.assertEquals(CreatePriority.objects.get(priority="3"), c2)
-
-    def test_create_priority4(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        c2 = CreatePriority.objects.create(classes=c458, priority="1")
-        self.assertEquals(CreatePriority.objects.get(priority="1"), c2)
-
-    def test_create_priority5(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        c2 = CreatePriority.objects.create(classes=c458, priority="1")
-        self.assertNotEquals(CreatePriority.objects.get(priority="1"), c1)
-
-    def test_create_priority5(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        c2 = CreatePriority.objects.create(classes=c458, priority="one")
-        self.assertNotEquals(CreatePriority.objects.get(priority="one"), None)
-
-    def test_save_priority1(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        c2 = CreatePriority.objects.create(classes=c458, priority="3")
-        pList = CreatePriority.objects.all()
-        sp1 = SavePriority.objects.create(user=santha, myList=pList)
-        self.assertEquals(SavePriority.objects.get(user=santha), sp1)
-
-    def test_save_priority2(self):
-        User.objects.create(email="skravi@uwm.edu", password="imaTA32!")
-        santha = User.objects.get(email="skravi@uwm.edu")
-        User.objects.create(email="stoffleb@uwm.edu", passord="iamTA55!")
-        bryan = User.objects.get(email="stoffelb@uwm.edu")
-        c361 = CourseTime.objects.create(department="CS", number="361", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c395 = CourseTime.objects.create(department="CS", number="395", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c458 = CourseTime.objects.create(department="CS", number="458", start="1100", end="1150", day="TTH", section="802", instructor="Rock")
-        c1 = CreatePriority.objects.create(classes=c361, priority="1")
-        c2 = CreatePriority.objects.create(classes=c458, priority="2")
-        c3 = CreatePriority.objects.create(classes=c395, priority="3")
-        sList = CreatePriority.objects.all()
-        bList = CreatePriority.objects.get(classes=c361)
-        sp1 = SavePriority.objects.create(user=santha, myList=sList)
-        sp2 = SavePriority.objects.create(user=bryan, myList=bList)
-        sp2.myList.add(c3)
-        self.assertEquals(SavePriority.objects.get(user=bryan).objects.get(classes=c395), c3)
 
 class InstructorTestCase(TestCase):
     pass
