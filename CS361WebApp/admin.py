8392e42d2c2011171b0d6c48ae61fa6ade144b48
Hojin Park <hojin@uwm.edu>
2019-12-18 15:22:23 -0600

    Added role (TA, instructor)

--- CS361WebApp/admin.py
+++ CS361WebApp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CourseTime, SavePriority, CreatePriority
-
+from user.models import Profile
 # Register your models here.
 # UserAdmin.list_display += ('role',)
+admin.site.register(Profile)
+admin.site.site_header = 'Administration'
 admin.site.register(CourseTime)
 admin.site.register(SavePriority)
 admin.site.register(CreatePriority)
